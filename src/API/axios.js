be910e6951d1cb2a51a4f511545f04c404187b6e
kiyaeh <kiyaehcs@gmail.com>
2024-04-04 20:47:01 +0300

    j

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-d8773/us-central1/api",
-  baseURL: "https://amazon-backend-4owt.onrender.com",
+  baseURL: "https://amazon-backend-4owt.onrender",
 });
